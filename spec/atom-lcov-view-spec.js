c03783959a602c705c13e838724c50bc5a40016d
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-05-19 18:15:41 +0700

    Initialize project

---
+++ spec/atom-lcov-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomLcovView from '../lib/atom-lcov-view';
+
+describe('AtomLcovView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
